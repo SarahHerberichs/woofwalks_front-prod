a6e325756fbe16b800e63ec7ba8706a46ccc3661
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-16 09:45:19 +0200

    fix: update .env and api.js and var in front env railway

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 import { setupInterceptors } from "./axiosInterceptors";
 
+const apiUrl = process.env.REACT_APP_API_URL;
 //Création instance axios et sa config par défaut
 const api = axios.create({
-  // baseURL: "https://localhost:8443/api",
-  baseURL: "https://woofwalksback-prod-production.up.railway.app/api",
-  withCredentials: true, 
+    baseURL: `${apiUrl}/api`,
+    withCredentials: true, 
 });
+// const api = axios.create({
+//   baseURL: "https://localhost:8443/api",
+//   // baseURL: "https://woofwalksback-prod-production.up.railway.app/api",
+//   withCredentials: true, 
+// });
 //intercepteur qui affiche toast si erreur
 setupInterceptors(api); 
 
