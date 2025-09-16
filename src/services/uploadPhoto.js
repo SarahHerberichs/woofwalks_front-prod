8492dc0a68b3e651aa7756ae3f6e26f7ac3fd6c1
SarahHerberichs <sarah.herberichs@neuf.fr>
2025-09-16 09:16:03 +0200

    fix: update api.js path

--- src/services/uploadPhoto.js
+++ src/services/uploadPhoto.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
 export const uploadPhoto = async (data) => {
-  const response = await api.post("api/main_photo", data);
+  const response = await api.post("/main_photo", data);
   return response.data;
 };
